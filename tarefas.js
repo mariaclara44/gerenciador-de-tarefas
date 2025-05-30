d94b5ef79faf3e47ead76c1e2e24205b34630c7d
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 08:29:30 -0300

    Adiciona novas tarefas

---
+++ tarefas.js
@@ -0,0 +1,19 @@
+let tarefas = ['Estudar JavaScript',
+    'Fazer exercícios de matemática', 
+    'Lavar a louça', 
+    'Comprar mercado', 
+    'Ir à academia',
+    'Ler um livro', 
+    'Fazer revisão para o teste', 
+    'Limpar o quarto', 
+    'Estudar para concurso', 
+    'Organizar o computador'];
+
+console.log(`Antes do push ${tarefas}`);
+
+// Adicionado dois novos elementos no array
+tarefas.push (`Fazer exercícios de português, Corrida`);
+
+
+console.log(`Depois do push: ${tarefas}`);
+
